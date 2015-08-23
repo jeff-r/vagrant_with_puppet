fe9a74ff0efe706977fd649af39f63c8dbf3e6be
Jeff Roush <jeff@jeffroush.com>
2015-08-23 11:10:48 -0700

    Add dotfiles

--- puppet/modules/default.pp
+++ puppet/modules/default.pp
@@ -16,9 +16,51 @@ node default {
     require => Package["nginx-full"],
   }
 
+  file{"/root/ez-dotfiles":
+    ensure  => link,
+    target  => "/vagrant/puppet/files/ez-dotfiles",
+  }
+  file{"/root/scripts":
+    ensure  => link,
+    target  => "/vagrant/puppet/files/ez-dotfiles/scripts",
+  }
+  file{"/root/.vim":
+    ensure  => link,
+    target  => "/vagrant/puppet/files/ez-dotfiles/vim/vim",
+  }
   file{"/root/.vimrc":
     ensure  => link,
-    target  => "/vagrant/puppet/files/.vimrc",
-    force => true,
+    target  => "/vagrant/puppet/files/ez-dotfiles/vim/vimrc",
+  }
+
+  file{"/root/.bashrc":
+    ensure  => link,
+    target  => "/vagrant/puppet/files/ez-dotfiles/bash/bashrc"
+  }
+  file{"/root/.bash_profile":
+    ensure  => link,
+    target  => "/vagrant/puppet/files/ez-dotfiles/bash/bash_profile"
+  }
+  file{"/root/.bash_aliases":
+    ensure  => link,
+    target  => "/vagrant/puppet/files/ez-dotfiles/bash/bash_aliases"
+  }
+  file{"/root/.bash_functions":
+    ensure  => link,
+    target  => "/vagrant/puppet/files/ez-dotfiles/bash/bash_functions"
+  }
+  file{"/root/.bash_path":
+    ensure  => link,
+    target  => "/vagrant/puppet/files/ez-dotfiles/bash/bash_path"
+  }
+
+  file{"/root/.gitconfig":
+    ensure  => link,
+    target  => "/vagrant/puppet/files/ez-dotfiles/git/gitconfig"
+  }
+
+  file{"/root/.tmux.conf":
+    ensure  => link,
+    target  => "/vagrant/puppet/files/ez-dotfiles/tmux/tmux.conf"
   }
 }
